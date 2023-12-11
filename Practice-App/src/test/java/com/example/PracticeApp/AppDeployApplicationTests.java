3b1732c0c35c02d9d80b8bb035815db79ae2acfe
Viviana Valentin <137527897+Viv-Valentin@users.noreply.github.com>
2023-12-11 16:33:38 -0500

    Add files via upload
    testing first commit

---
+++ Practice-App/src/test/java/com/example/PracticeApp/AppDeployApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PracticeApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppDeployApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
