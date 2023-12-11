5e55d6b5288e28b248a6430a68be58baa631fc5e
XxVivianaxX <viviana.valentin1230@gmail.com>
2023-12-11 16:54:44 -0500

    seeing if this push will work

---
+++ src/main/java/com/example/PracticeApp/customer/Customer.java
@@ -0,0 +1,14 @@
+package com.example.PracticeApp.customer;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+@Data
+@AllArgsConstructor
+public class Customer {
+
+    private Long customerId;
+    private String firstName;
+    private String lastName;
+    private String email;
+
+}
